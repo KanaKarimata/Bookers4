583b4428d79296cf8d71b38d1a8c4dc46c94f360
KanaKarimata <kana.lt22@gmail.com>
2022-05-10 01:23:50 +0000

    [Finish]応用課題8c

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,9 +1,9 @@
 class Group < ApplicationRecord
   belongs_to :owner, class_name: 'User'
   has_many :group_users, dependent: :destroy
-  has_one_attached :image
 
-  # has_many :group_images, dependent: :destroy
+  has_many :users, through: :group_users, source: :user
+  has_one_attached :image
 
   validates :name, presence: true
   validates :introduction, presence: true
