7f937facec5be965386863f89abf1dbc0927e991
KanaKarimata <kana.lt22@gmail.com>
2022-05-10 05:06:54 +0000

    [Add]mailer

--- app/mailers/notice_events_mailer.rb
+++ app/mailers/notice_events_mailer.rb
@@ -1,3 +1,22 @@
 class NoticeEventsMailer < ApplicationMailer
+  def sent_notification(member, event)
+    @group = event[:group]
+    @title = event[:title]
+    @body = event[:body]
+    
+    @mail = NoticeEventsMailer.new()
+    mail (
+      from: ENV['MAIL_ADDRESS']
+      to: member.email,
+      subject: 'New Event Notice!'
+    )
+  end
+  
+  def self.send_notifications_to_group(event)
+    group = event[:group]
+    group.users.each do |member|
+      NoticeEventsMailer.sent_notification(member, event).deliver_now
+    end
+  end
   
 end
